c73bc05f58feeb244a1734add650791d8f855090
Matthew Harrigan <matthew.harrigan@bbc.co.uk>
2021-12-12 19:56:34 +0000

    Finally got it working with BFS

--- 09/Day9.js
+++ 09/Day9.js
@@ -3,7 +3,7 @@ const input = readFile("input.txt", "utf-8").split("\n");
 
 const grid = input.map((row) => Array.from(row));
 
-const results = [];
+const lowestPoints = [];
 
 grid.forEach((row, rIndex) => {
   row.forEach((element, cIndex) => {
@@ -13,10 +13,64 @@ grid.forEach((row, rIndex) => {
     const left = grid[rIndex][cIndex - 1] ? grid[rIndex][cIndex - 1] : 11;
 
     if (element < up && element < right && element < down && element < left) {
-        results.push(parseInt(element ));
+      lowestPoints.push(`${rIndex},${cIndex}`);
     }
-
   });
 });
 
-console.log(results.map(x => x + 1).reduce((acc, cur) => acc + cur));
+function search(start) {
+  const queue = [];
+  queue.push(start);
+
+  const visited = [];
+  visited.push(start);
+
+  // BFS
+  while (queue.length > 0) {
+    const currElement = queue.shift();
+
+    let [rIndex, cIndex] = currElement.split(",").map((_) => parseInt(_));
+
+    // up
+    if (grid[rIndex - 1] && grid[rIndex - 1][cIndex] < 9) {
+      let key = [rIndex - 1, cIndex].join(",");
+      if (!visited.includes(key)) {
+        queue.push(key);
+        visited.push(key);
+      }
+    }
+
+    //right
+    if (grid[rIndex][cIndex + 1] && grid[rIndex][cIndex + 1] < 9) {
+      let key = [rIndex, cIndex + 1].join(",");
+      if (!visited.includes(key)) {
+        queue.push(key);
+        visited.push(key);
+      }
+    }
+
+    // down
+    if (grid[rIndex + 1] && grid[rIndex + 1][cIndex] < 9) {
+      let key = [rIndex + 1, cIndex].join(",");
+      if (!visited.includes(key)) {
+        queue.push(key);
+        visited.push(key);
+      }
+    }
+
+    // left
+    if (grid[rIndex][cIndex - 1] && grid[rIndex][cIndex - 1] < 9) {
+      let key = [rIndex, cIndex - 1].join(",");
+      if (!visited.includes(key)) {
+        queue.push(key);
+        visited.push(key);
+      }
+    }
+  }
+
+  return visited.length;
+}
+
+const result = lowestPoints.map(_ => search(_)).sort((a, b) => b - a).splice(0, 3).reduce((acc, cur) => acc * cur);
+
+console.log(result)
